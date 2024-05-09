d0cf46807c4596a59d7249184ccec85a4399ad1e
Taewon Hwang <htw1203@gmail.com>
2024-05-09 19:26:41 +0900

    menu 가격 형식 변경

--- src/utils/Utils.jsx
+++ src/utils/Utils.jsx
@@ -4,3 +4,16 @@ export const GetPath = () => {
     const temp = pathnameArray[1]
     return temp.charAt(0).toUpperCase() + temp.slice(1)
 }
+
+export const formatPrice = (price) => {
+    const priceArray = price.toString().split('').reverse()
+    const formattedPrice = priceArray.map((digit, index) => {
+        if (index !== 0 && index % 3 === 0) {
+            return digit + ','
+        } else {
+            return digit
+        }
+    }).reverse().join('') + '원'
+
+    return formattedPrice
+}
