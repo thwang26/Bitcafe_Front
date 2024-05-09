e2ab829285c872b9a5508bc906961c4de9d6ad05
Taewon Hwang <htw1203@gmail.com>
2024-05-09 19:27:42 +0900

    bottomnavigation 수정

--- src/components/BottomNavigation.jsx
+++ src/components/BottomNavigation.jsx
@@ -10,42 +10,41 @@ const BottomNavigation = () => {
 	const { IcMain, IcOrder, IcCart, IcOthers } = useIcon()
 	const [activeIcon, setActiveIcon] = useState()
 
-	const handleIconClick = (iconName) => {
-		setActiveIcon(iconName)
-		if (iconName === 'Home') {
-			navigate('/')
-		} else {
-			navigate(`/${iconName.toLowerCase()}`)
-		}
+	const handleIconClick = (nav) => {
+			navigate(`/${nav}`)
 	} // 아이콘 클릭 시 작동
 
 	useEffect(() => {
         setActiveIcon(GetPath() || 'Home')
-    }, []) // bottomnavigation 랜더링 시 작동, path가 비어있으면 home
+    }, [navigate]) // navigate 값이 바뀔 때 적용, path가 비어있으면 home
 
 	const BOTTOM_NAVIGATION_MENU_LIST = [
 		{
 			title: 'Home',
 			icon: <IcMain size={icon.bottomNavigation} color={activeIcon === 'Home' ? palette.activeIcon : palette.icon} />,
+			navigate: ''
 		},
 		{
 			title: 'Order',
 			icon: <IcOrder size={icon.bottomNavigation} color={activeIcon === 'Order' ? palette.activeIcon : palette.icon} />,
+			navigate: 'order'
 		},
 		{
 			title: 'Cart',
 			icon: <IcCart size={icon.bottomNavigation} color={activeIcon === 'Cart' ? palette.activeIcon : palette.icon} />,
+			navigate: 'cart'
 		},
 		{
 			title: 'Others',
 			icon: <IcOthers size={icon.bottomNavigation} color={activeIcon === 'Others' ? palette.activeIcon : palette.icon} />,
+			navigate: 'others'
 		},
 	]
 
 	return (
 		<S.Main>
 			{BOTTOM_NAVIGATION_MENU_LIST.map(item => (
-				<S.WrapIcon key={item.title} onClick={() => handleIconClick(item.title)}>
+				<S.WrapIcon key={item.title} onClick={() => handleIconClick(item.navigate)}>
 					{item.icon}
 					<S.Text>{item.title}</S.Text>
 				</S.WrapIcon>
